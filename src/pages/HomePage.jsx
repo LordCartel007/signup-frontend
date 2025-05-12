5a0cd9ea65d66a66450facc3b6590ccdecdf9523
LordCartel007 <cartellord77@gmail.com>
2025-05-12 03:53:26 +0100

    adjusted home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,6 +9,57 @@ const StyledDiv = styled.div`
   }
 `;
 
+const Title = styled.h1`
+  font-weight: bolder;
+
+  margin: 0;
+  color: #ffc107;
+  text-decoration: none;
+  font-family: "Rye", serif;
+  font-weight: 400;
+  font-style: normal;
+  flex: 1; /* Makes title fill available space */
+  justify-content: center;
+
+  @media screen and (max-width: 767px) {
+    font-size: 2rem;
+  }
+
+  @media screen and (min-width: 767px) {
+    font-size: 3rem;
+  }
+
+  /* nest hub tv screen */
+  @media screen and (min-width: 1023px) {
+    font-size: 4rem;
+  }
+`;
+
+const Pwords = styled.p`
+  font-weight: bolder;
+
+  margin: 0;
+  color: #ffc107;
+  text-decoration: none;
+  font-family: "Rye", serif;
+  font-weight: 400;
+  font-style: normal;
+  flex: 1; /* Makes title fill available space */
+  justify-content: center;
+
+  @media screen and (max-width: 767px) {
+    font-size: 1rem;
+  }
+
+  @media screen and (min-width: 767px) {
+    font-size: 1.5rem;
+  }
+
+  /* nest hub tv screen */
+  @media screen and (min-width: 1023px) {
+    font-size: 1.5rem;
+  }
+`;
 const PathButton = styled.button`
   border: 0;
 
@@ -19,7 +70,7 @@ const PathButton = styled.button`
   text-decoration: none;
 
   background-color: #0e0e10;
-  color: #ffe600;
+  color: #ffc107;
   padding: 10px;
   font-size: 3vh;
   height: 80px;
@@ -47,21 +98,21 @@ const HomePage = () => {
         </video>
         <div className="content">
           <h1 className="heading-top font-extrabold">Cartel Ai</h1>
-          <div className="text-word font-extrabold">
-            <p>The Ai You can trust</p>
-            <p>
+          <div className="">
+            <Title>The Ai You can trust</Title>
+            <Pwords>
               {" "}
               Cartel Ai can talk , read Pdf or any file you input , she can open
               websites with voice recognition, you can gist with her and she
               says the best joke,
-            </p>
-            <p>
+            </Pwords>
+            <Pwords>
               She can solve maths equation if you send her the pics, So she is a
               chat and speak bot
-            </p>
-            <p>
+            </Pwords>
+            <Pwords>
               Use the sign up / login button to have access to the bot for free
-            </p>
+            </Pwords>
           </div>
         </div>
         <div className="flex justify-center font-extrabold ">
