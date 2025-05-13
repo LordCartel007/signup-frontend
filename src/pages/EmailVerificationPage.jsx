0459c14eb2b236927611bfa5626b2e6ff3667ef1
LordCartel007 <cartellord77@gmail.com>
2025-05-13 14:50:13 +0100

    changed link

--- src/pages/EmailVerificationPage.jsx
+++ src/pages/EmailVerificationPage.jsx
@@ -114,7 +114,7 @@ const EmailVerificationPage = () => {
     <StyledDiv className="containervideo">
       <video autoPlay loop muted playsInline className="background-clip">
         <source
-          src="https://cartel-next-ecommerce.s3.eu-north-1.amazonaws.com/liquid+ink+cut+video.mp4"
+          src="https://cartel-next-ecommerce.s3.eu-north-1.amazonaws.com/satisfactory+desserts+video.mp4"
           type="video/mp4"
         />
       </video>
